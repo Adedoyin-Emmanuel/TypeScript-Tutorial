e59c4ec67e50c339321d28e2f878317c0ce69225
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-23 16:12:31 +0100

    Added the folder hello world

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,11 @@
 "use strict";
-let age = 35;
-console.log(age);
+const user_1 = { name: "Emmanuel", age: 17, married: true };
+const Emp1 = {
+    firstName: "Emmanuel",
+    lastName: "Adedoyin",
+    age: 17,
+    drive: () => {
+        return "i am driving";
+    }
+};
+console.log(Emp1.drive());
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,49 @@
+//using type in typescripts
 type User = {
     name: string,
     age: number
+    married ?: boolean
 };
 
-const user_1 : User = { name : "Emmanuel", age: 17}
+const user_1 : User = { name : "Emmanuel", age: 17, married: true}
 
-console.log(user_1.name);
+
+//using interfaces
+
+interface Employee {
+    firstName: string,
+    lastName: string,
+    age: number,
+    drive: () => string,
+    
+}
+
+interface Driver extends Employee{
+    job: string,
+    carType: string
+}
+
+
+const Emp1: Employee = {
+    firstName : "Emmanuel",
+    lastName:"Adedoyin",
+    age: 17,
+    drive: () =>{
+        return "i am driving"
+    }
+}
+
+
+console.log(Emp1.drive());
+
+
+const Driver1: Driver = {
+    firstName : "Adeniyi",
+    lastName:"Adedoyin",
+    age: 20,
+    drive: () =>{
+        return "i am riding"
+    },
+    job:"Driver",
+    carType:"Ferarri",
+}
