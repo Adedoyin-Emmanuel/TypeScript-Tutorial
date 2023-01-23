6d2b8263961c6b893d504aab65d6dac34fdf06e1
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2023-01-23 17:26:04 +0100

    Updated the 0x02 folder

---
+++ src/0x02-Type_Declaration/type-declaration.ts
@@ -0,0 +1,17 @@
+//a variable with a string type
+const my_name: string = "Adedoyin Emmanuel";
+
+//a variable with a number type
+const my_age: number  = 17;
+
+//a variable with an array of strings type
+const my_friends: string[] = ["Henry", "Chidi", "Tomi", "Kareem", "Folu", "Joseph", "Oyindamola", "Akpan", "Seun", "Temmy", "Gift", "Sarah", "Idowu", "Afeez", "Aniefiok", "Daniel", "Eniola"];
+
+//a variable with an object type
+const users : object = {
+    id: 1,
+    first_name: "Adedoyin",
+    last_name:"Emmanuel",
+    age:17,
+    hobbies:["coding","travelling","eating"]
+}
